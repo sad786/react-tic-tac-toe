da57d3ebf84fd39c70589f765cb96577119dcce1
Saddam Ahmed <sa522408@gmail.com>
2025-01-22 12:09:40 +0530

    Update App.js
    added analytics for page visitors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css'
+import { Analytics } from '@vercel/analytics/next';
 
 /*
 function Square({ value, onSquareClick }) {
@@ -202,6 +203,7 @@ export default function Game() {
       <div className="game-info">
         <ol>{moves}</ol>
       </div>
+	  <Analytics />
     </div>
   );
 }
