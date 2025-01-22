2a29242a35870f14d244cedf20148bada9c03377
Saddam Ahmed <sa522408@gmail.com>
2025-01-22 12:16:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css'
-import { Analytics } from '@vercel/analytics/next';
+//import { Analytics } from '@vercel/analytics/next';
 
 /*
 function Square({ value, onSquareClick }) {
@@ -203,7 +203,7 @@ export default function Game() {
       <div className="game-info">
         <ol>{moves}</ol>
       </div>
-	  <Analytics />
+	  /*<Analytics /> */
     </div>
   );
 }
